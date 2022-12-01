c6f1be74ec22e873dd9f8be96fcbec04bf7a2005
Vili Heikkilä <vili.heikkila@zalando.fi>
2022-12-01 23:21:41 +0200

    desparate attempts

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,5 @@
-mod utils {
-    use std::fs::File;
-    use std::io::{BufRead, BufReader};
-
-    pub fn read_lines(path: &str) -> Vec<String> {
-        let file = File::open(path).expect("no such file");
-        let buff = BufReader::new(file);
-        buff.lines()
-            .map(|x| x.expect("unable to parse line"))
-            .collect()
-    }
-}
+use crate::utils::file;
+pub mod utils;
 
 struct Elf {
     foods: Vec<i32>
@@ -42,7 +32,7 @@ fn to_elves(sv: Vec<String>) -> Vec<Elf> {
 }
 
 fn part1() {
-    let raw = utils::read_lines("./inputs/day1.txt");
+    let raw = file::read_lines("./inputs/day1.txt");
     let elves: Vec<Elf> = to_elves(raw);
     let fat_elf = elves
         .iter()
@@ -54,7 +44,7 @@ fn part1() {
 
 
 fn part2() {
-    let raw = utils::read_lines("./inputs/day1.txt");
+    let raw = file::read_lines("./inputs/day1.txt");
     let elves: Vec<Elf> = to_elves(raw);
     let mut foods: Vec<i32> = elves
         .iter()
