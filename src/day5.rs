3190a1c87e756e112d6a3464b36b681e9a7b9a17
Vili Heikkilä <vili.heikkila@zalando.fi>
2022-12-05 21:29:33 +0200

    d5 top part parsed

--- src/day5.rs
+++ src/day5.rs
@@ -1,9 +1,14 @@
+use std::ops::Add;
 use crate::utils;
 use std::str;
 
-
+#[derive(Debug)]
 struct Stacks {
     towers: Vec<Vec<String>>
+}
+
+fn new_stacks(towers: Vec<Vec<String>>) -> Stacks {
+    return Stacks { towers }
 }
 
 fn byte_to_letter(c: u8) -> String {
@@ -19,6 +24,7 @@ fn parse_tower_by_number(top_raw: &Vec<String>, number: i32) -> Vec<String> {
     let without_first = &top_raw[1..];
 
     let stack: Vec<String> = without_first.into_iter()
+        .map(|x| right_pad(x.clone(), (tower_index +1) as i32))
         .map(|x| x.as_bytes()[tower_index])
         .map(|x| byte_to_letter(x))
         .take_while(|x| !x.trim().is_empty())
@@ -41,29 +47,34 @@ fn count_towers(top_rows: &Vec<String>) -> i32 {
     return last_tower_number;
 }
 
-fn parse_top(raw: Vec<String>) {
+fn right_pad(s: String, required_len: i32) -> String {
+    let current_len = s.len();
+    let required_spaces = required_len  - current_len as i32;
+    if required_spaces < 0 {
+        return s
+    }
+    let spaces = " ".repeat(required_spaces as usize);
+    return s.add(spaces.as_str());
+}
+
+fn parse_top(raw: Vec<String>) -> Stacks {
     let mut top_rows: Vec<String> = raw.into_iter().take_while(|x| x.len() > 1)
         .collect();
-
     top_rows.reverse();
 
     let number_of_towers = count_towers(&top_rows);
-
-    println!("{}", number_of_towers);
     let towers: Vec<Vec<String>> = (1..number_of_towers + 1).into_iter()
         .map(|x| parse_tower_by_number(&top_rows, x))
         .collect();
 
-
-    for x in towers {
-        println!("{:?}", x)
-    }
+    return new_stacks(towers)
 }
 
 pub fn part1() {
     let raw = utils::read_lines("./inputs/day5.txt");
 
+    let stacks = parse_top(raw);
 
-    parse_top(raw)
+    println!("{:?}", stacks)
 
 }
